4a3932005ec7804c9d69a79dcc5128c81099beda
王超 <wangchao@tv365.net>
2018-09-11 13:03:56 +0800

    显示case内容

--- UnitTest.h
+++ UnitTest.h
@@ -20,39 +20,41 @@
 #define UT_STR(s) __UT_STR__(s)
 #endif
 
-#define PASSED_LOG(fmt, ...) \
-printf("\e[1m\e[38;5;14m✔ %s:%d %s> passed. " fmt "\e[0m\n",\
- __FILENAME__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
-#define FAILED_LOG(fmt, ...)  \
-printf("\e[1m\e[38;5;196m✖ %s:%d %s> failed. " fmt "\e[0m\n", \
-__FILENAME__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
+#define PASSED_LOG(case_, fmt, ...) \
+    printf("\e[1m\e[38;5;14m✔ %s:%d %s> %s passed. " fmt "\e[0m\n",\
+        __FILENAME__, __LINE__, __FUNCTION__, case_, ##__VA_ARGS__)
+
+#define FAILED_LOG(case_, fmt, ...)  \
+    printf("\e[1m\e[38;5;196m✖ %s:%d %s> %s failed. " fmt "\e[0m\n", \
+        __FILENAME__, __LINE__, __FUNCTION__, case_, ##__VA_ARGS__)
+
 
 #define UT_ASSERT(c, fmt, ...) \
     if((c)){\
-        PASSED_LOG(fmt,##__VA_ARGS__);\
+        PASSED_LOG(UT_STR(c), "");\
     }else{\
-        FAILED_LOG("expect:%s, " fmt, UT_STR(c)  ##__VA_ARGS__);\
+        FAILED_LOG(UT_STR(c), fmt, ##__VA_ARGS__);\
     }
 
 #define UT_ASSERT_LESS(a, b, fmt, ...) \
     if((a)<(b)){\
-        PASSED_LOG(fmt,##__VA_ARGS__);\
+        PASSED_LOG(UT_STR(a < b), "");\
     }else{\
-        FAILED_LOG("%s",fmt,##__VA_ARGS__);\
+        FAILED_LOG(UT_STR(a < b), fmt, ##__VA_ARGS__);\
     }
 
 #define UT_ASSERT_EQUAL(a, b, fmt, ...) \
     if((a)==(b)){\
-        PASSED_LOG(fmt,##__VA_ARGS__);\
+        PASSED_LOG(UT_STR(a == b), "");\
     }else{\
-        FAILED_LOG(fmt,##__VA_ARGS__);\
+        FAILED_LOG(UT_STR(a == b), fmt, ##__VA_ARGS__);\
     }
 
 #define UT_ASSERT_GRATER(a, b, fmt, ...) \
     if((a)>(b)){\
-        PASSED_LOG(fmt,##__VA_ARGS__);\
+        PASSED_LOG(UT_STR(a > b), "");\
     }else{\
-        FAILED_LOG(fmt,##__VA_ARGS__);\
+        FAILED_LOG(UT_STR(a > b), fmt, ##__VA_ARGS__);\
     }
 #endif
 
